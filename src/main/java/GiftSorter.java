32669dcfaa50f7d362ae22d5fd2f062021faa208
Adam Pridmore <adam_pridmore@hotmail.com>
2018-02-07 22:54:49 +0000

    - Removed old java code.

--- src/main/java/GiftSorter.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class GiftSorter{
-    public String sortGiftCode(String code){
-        char[] chars = code.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
-    }
-}
--- src/test/java/SquareDigitTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-public class SquareDigitTest {
-    @Test
-    public void test() {
-        assertEquals(811181, new SquareDigit().squareDigits(9119));
-    }
-}
